1fa44a698447fc2003d91fd0f8769364c1b45ebd
xzwb <1478863479@qq.com>
2020-05-09 17:34:53 +0800

    test(pom.xml): create and test
    创建一个新的springBoot项目以及写一个controller测试，测试日志

---
+++ src/main/java/cc/xzwb/bookstore/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cc.xzwb.bookstore.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.debug("hello测试");
+        return "hello 你好";
+    }
+}
